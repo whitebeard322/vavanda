9d1779c0f672da49f0be096b155d8d5c904a7c8c
whitebeard322 <163688179+whitebeard322@users.noreply.github.com>
2024-04-24 17:13:45 +0300

    Update reflink.js

--- js/reflink.js
+++ js/reflink.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $("a").click(function(){
-    $("#reflink*").attr("href", "https://vavadapartner.pro/?promo=09eb7607-2927-4d3a-aa93-0f417cbc2d62&target=register");
+    $("#reflink*").attr("href", "https://partnervavadarv.com/?promo=09eb7607-2927-4d3a-aa93-0f417cbc2d62&target=register");
   });
 });
